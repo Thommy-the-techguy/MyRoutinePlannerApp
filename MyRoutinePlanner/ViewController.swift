710faa13e587e3ea3bdc0444adcd472fad6c94e4
Artem <chizhik.artem@yandex.by>
2024-01-14 15:41:03 +0300

    Initial Commit

---
+++ MyRoutinePlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyRoutinePlanner
+//
+//  Created by Артем Чижик on 14.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
